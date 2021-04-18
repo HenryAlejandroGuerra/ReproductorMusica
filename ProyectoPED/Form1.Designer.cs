fb48d30a7dc1e8a3ff31325a1e7b1574444bd310
Henry Martinez <henryalejandromar@gmail.com>
2021-04-17 18:06:21 -0600

    init

---
+++ ProyectoPED/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace ProyectoPED
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Playlist = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Playermusic = new AxWMPLib.AxWindowsMediaPlayer();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Playermusic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 41);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Harlow Solid Italic", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(250, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(201, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Logo o nombre de reproductor";
+            // 
+            // Playlist
+            // 
+            this.Playlist.FormattingEnabled = true;
+            this.Playlist.Location = new System.Drawing.Point(616, 47);
+            this.Playlist.Name = "Playlist";
+            this.Playlist.Size = new System.Drawing.Size(172, 290);
+            this.Playlist.TabIndex = 1;
+            this.Playlist.SelectedIndexChanged += new System.EventHandler(this.Playlist_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.OrangeRed;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(616, 343);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 55);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Select Songs";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Playermusic
+            // 
+            this.Playermusic.Enabled = true;
+            this.Playermusic.Location = new System.Drawing.Point(25, 47);
+            this.Playermusic.Name = "Playermusic";
+            this.Playermusic.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("Playermusic.OcxState")));
+            this.Playermusic.Size = new System.Drawing.Size(566, 351);
+            this.Playermusic.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Playermusic);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Playlist);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Music Plyer UDB";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Playermusic)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox Playlist;
+        private System.Windows.Forms.Button button1;
+        private AxWMPLib.AxWindowsMediaPlayer Playermusic;
+    }
+}
+
---
+++ ProyectoPED/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoPED
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Crear variables globales tipo String para guardar los titulos o nombres de las canciones
+        String[] paths, files;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //codigo para seleccionar caciones
+            OpenFileDialog ofd = new OpenFileDialog();
+            //codigo para seleccionar varias canciones
+            
+            ofd.Multiselect = true;
+
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK) 
+            {
+                files = ofd.SafeFileNames; // Guarda los nombres de los archivos
+                paths = ofd.FileNames;//Guarda las rutas de los  de las caciones
+                //Muesta los totulos de las canciones de en la lista
+
+                for(int i = 0;i < files.Length; i++)
+                {
+                    Playlist.Items.Add(files[i]); //muestra las canciones en la lista
+                }
+            }
+
+        }
+
+        private void Playlist_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //codigo para reproducir musica
+            Playermusic.URL = paths[Playlist.SelectedIndex];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
